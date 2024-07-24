6215d70014d0e83fa5f2f684b0a105af161aa4be
Ivan James Estores <ivanjamesfiles@gmail.com>
2024-07-24 23:15:19 +0800

    First Commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+import Hero from '../../components/hero/Hero'
+
+export default function Home() {
+  return (
+    <div>
+    
+    <Header />
+    <Hero />
+    Home, itong text na ito kay nasa Home na jsx 
+
+    <h1> Dito ka ga tawag ng header, hero, footer etc</h1>
+
+    </div>
+  )
+}
