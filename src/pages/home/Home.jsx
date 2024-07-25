b4124201666a3d54364ed47de0f115c484cdc0af
Olsen John Gabriel <152185216+OlsenJohnGab@users.noreply.github.com>
2024-07-25 11:37:08 +0800

    Added the Footer in the Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../../components/header/Header'
 import Hero from '../../components/hero/Hero'
+import Footer from '../../components/footer/Footer'
 
 export default function Home() {
   return (
@@ -8,9 +9,7 @@ export default function Home() {
     
     <Header />
     <Hero />
-    Home, itong text na ito kay nasa Home na jsx 
-
-    <h1> Dito ka ga tawag ng header, hero, footer etc</h1>
+    <Footer/>
 
     </div>
   )
