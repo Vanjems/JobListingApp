3bad3bcbbf0c30000f58d129a27d331e5344b354
Olsen John Gabriel <152185216+OlsenJohnGab@users.noreply.github.com>
2024-07-25 11:22:23 +0800

    Updated Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './pages/home/Home'
+import Footer from './components/footer/Footer'
 
 export default function App() {
   return (
     <div>
       <Home />
+      <Footer/>
     </div>
   )
 }
